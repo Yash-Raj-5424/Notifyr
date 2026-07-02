9c050ee393ece3b61facfd88aa356c16231fc836
Yash-Raj-5424 <myash3499@gmail.com>
2026-07-02 23:21:23 +0530

    initial project setup

---
+++ src/main/java/com/yash/Notifyr/NotifyrApplication.java
@@ -0,0 +1,13 @@
+package com.yash.Notifyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/Notifyr/NotifyrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.Notifyr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
