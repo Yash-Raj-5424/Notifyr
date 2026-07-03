f8699622167c0023123dd7d810f28b94f455053a
Yash-Raj-5424 <myash3499@gmail.com>
2026-07-03 18:41:25 +0530

    fix: resolved redis serialization with NotificationResponse

--- src/main/java/com/yash/Notifyr/config/RedisConfig.java
+++ src/main/java/com/yash/Notifyr/config/RedisConfig.java
@@ -1,11 +1,14 @@
 package com.yash.Notifyr.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.yash.Notifyr.dto.NotificationResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -15,9 +18,15 @@ public class RedisConfig {
     public RedisTemplate<String, NotificationResponse> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, NotificationResponse> template = new RedisTemplate<>();
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        Jackson2JsonRedisSerializer<NotificationResponse> jsonSerializer =
+            new Jackson2JsonRedisSerializer<>(objectMapper, NotificationResponse.class);
+
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(jsonSerializer);
 
         return template;
     }
